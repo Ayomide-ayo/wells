74b6fdbba0dad2049a7b0c7531f80cb0ae4b32f0
Ayomide-ayo <odemuyiwatitilayo744@gmail.com>
2025-05-11 17:02:13 -0500

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// export default {
+//   content: ["./index.html"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         wells: ['"Wells Fargo Sans"', 'Verdana', 'Arial', 'Helvetica', 'sans-serif'],
+//         verdana: ['Verdana', 'Arial', 'Helvetica', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+// tailwind.config.js
+export default {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        wells: ['"Wells Fargo Sans"', 'Verdana', 'Arial', 'Helvetica', 'sans-serif'],
+        verdana: ['Verdana', 'Arial', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
